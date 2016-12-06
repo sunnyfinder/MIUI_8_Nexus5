9f2779e78ed715e8fcf81fc5103e0613760c5371
Diorgenes <dyogenes_gd@hotmail.com>
2016-12-06 18:51:04 -0300

    Added ...patch for remove searchbar on statusbar ...missing symlinks for messages notifications

--- customize_miui_app.sh
+++ customize_miui_app.sh
@@ -38,6 +38,10 @@ function mergyXmlPart() {
 	done
 }
 
+if [ $1 = "MiuiSystemUI" ];then
+    $XMLMERGYTOOL $1/res/values $2/res/values
+fi
+
 if [ $1 = "SecurityCenter" ];then
     applyPatch $1 $2
 	sed -i '/- 16/a\sdkInfo:\n  minSdkVersion: '\''23'\''\n  targetSdkVersion: '\''23'\''' $2/apktool.yml
